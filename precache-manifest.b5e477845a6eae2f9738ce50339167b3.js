123a6fd9d60ba6a72a38d30e14c1242c8f762039
GAURAV BHAVSAR <95301011+GauravBhavsar9@users.noreply.github.com>
2022-02-05 00:10:42 +0530

    Deploying My First Website 0.1.0

---
+++ precache-manifest.b5e477845a6eae2f9738ce50339167b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630033911bf9d4d9d09ce5a45e9ebeee",
+    "url": "/Genius-Utils/index.html"
+  },
+  {
+    "revision": "4700bfaac4ed66288b2b",
+    "url": "/Genius-Utils/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/Genius-Utils/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "63fbe989101a4b7a3114",
+    "url": "/Genius-Utils/static/js/3.13844b13.chunk.js"
+  },
+  {
+    "revision": "4700bfaac4ed66288b2b",
+    "url": "/Genius-Utils/static/js/main.3ccc17f9.chunk.js"
+  },
+  {
+    "revision": "b8efe75680cca3a0fe26",
+    "url": "/Genius-Utils/static/js/runtime~main.20d0a781.js"
+  }
+]);
